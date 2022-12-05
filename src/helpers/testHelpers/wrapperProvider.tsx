15fb6512b0e8c70a037748968abf27908cf4f225
Merkaln_Bogdan <piccasoul19@ukr.net>
2022-12-05 15:23:27 +0200

    added test fixed wrapper provider for test

--- src/helpers/testHelpers/wrapperProvider.tsx
+++ src/helpers/testHelpers/wrapperProvider.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import "jest-styled-components";
+import { render } from "@testing-library/react";
 
 import { Provider } from "react-redux";
 
 import { store } from "redux/store";
 
-const wrapperProvider = (tree: React.ReactElement) => (
-  <Provider store={store}>
-    <Router>{tree}</Router>
-  </Provider>
-);
+const wrapperProvider = (children: React.ReactElement) =>
+  render(
+    <Provider store={store}>
+      <Router>{children}</Router>
+    </Provider>
+  );
 
 export { wrapperProvider };
--- src/page/Board/Board.test.tsx
+++ src/page/Board/Board.test.tsx
@@ -1,8 +1,17 @@
 import { wrapperProvider } from "helpers/testHelpers/wrapperProvider";
+
 import { Board } from "./Board";
 
 describe("render Board page", () => {
   it("Should render Board component", () => {
     wrapperProvider(<Board />);
+  });
+
+  it("Should render title", () => {
+    const { getByText } = wrapperProvider(<Board />);
+
+    const title = getByText(/weather viewer/i);
+
+    expect(title).toBeInTheDocument();
   });
 });
